0227fc2bb5649adeacd0c84fe5cc04276caa616d
Samsazar <l.alexiosan@gmail.com>
2022-11-01 16:04:38 +0300

    Добавление главного рута

--- index.js
+++ index.js
@@ -6,5 +6,15 @@ app.use(express.json({ extended: false }));
 
 app.use("/api/product", product);
 
+app.get("/", async (req, res) => {
+    try {
+      res.send("bombino")
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send("Server error");
+    }
+})
+
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
