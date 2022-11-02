8f3fc29b46d0c00ce1f8398c38f50f8fb876c999
Samsazar <l.alexiosan@gmail.com>
2022-11-02 22:54:12 +0300

    Тестирование DB через регистрацию 1.7

--- src/database/initdb.js
+++ src/database/initdb.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 async function connect() {
-    const db = "mongodb+srv://vercel-admin-user:sZPp54OVjWZ3Oey7@cluster0.ou28rrb.mongodb.net/?retryWrites=true&w=majority"
+    const db = "mongodb+srv://vercel-admin-user:wuULPQ1Vo6EhQoJD@cluster0.ou28rrb.mongodb.net/?retryWrites=true&w=majority"
     mongoose
         .connect(db, {useNewUrlParser: true, useUnifiedTopology: true })
         .then((res) => console.log('Connected to DB'))
