30d028f9871ac455b4c7f8e629282b0d8eb203e9
Samsazar <l.alexiosan@gmail.com>
2022-11-02 14:46:20 +0300

    Тестирование на подключение DB к проекту 1.3

--- src/database/initdb.js
+++ src/database/initdb.js
@@ -1,6 +1,9 @@
-import {MongoClient} from "mongodb"
+const mongoose = require("mongoose")
 
-const mongo = MongoClient.connect(process.env.MONGODB),
-    db = mongo.db(process.env.DB);
+const db = "mongodb+srv://vercel-admin-user:sZPp54OVjWZ3Oey7@cluster0.ou28rrb.mongodb.net/?retryWrites=true&w=majority"
+mongoose
+    .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((res) => console.log('Connected to DB'))
+    .catch((error) => console.log(error))
 
 module.exports = { db }
